5b37d542785dbda7f0c11817c5eb9f882502a06c
Waga <wagaodongo@gmail.com>
2019-11-07 15:26:55 +0300

    header layout

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,5 +1,73 @@
+import { Menu, Container, Image, Icon } from "semantic-ui-react";
+import Link from "next/link";
+
 function Header() {
-  return <>Header</>;
+  const user = true;
+
+  return (
+    <Menu fluid id="menu" inverted>
+      <Container text>
+        <Link href="/">
+          <Menu.Item header>
+            <Image
+              size="mini"
+              src="/static/logo.svg"
+              style={{ marginRight: "1em" }}
+            />
+            ReactReserve
+          </Menu.Item>
+        </Link>
+
+        <Link href="/cart">
+          <Menu.Item header>
+            <Icon name="cart" size="large" />
+            Cart
+          </Menu.Item>
+        </Link>
+
+        {user && (
+          <Link href="/create">
+            <Menu.Item header>
+              <Icon name="add square" size="large" />
+              Create
+            </Menu.Item>
+          </Link>
+        )}
+
+        {user ? (
+          <>
+            <Link href="/account">
+              <Menu.Item header>
+                <Icon name="user" size="large" />
+                Account
+              </Menu.Item>
+            </Link>
+
+            <Menu.Item header>
+              <Icon name="sign out" size="large" />
+              Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <>
+            <Link href="/login">
+              <Menu.Item header>
+                <Icon name="sign in" size="large" />
+                Login
+              </Menu.Item>
+            </Link>
+
+            <Link href="/signup">
+              <Menu.Item header>
+                <Icon name="signup" size="large" />
+                Signup
+              </Menu.Item>
+            </Link>
+          </>
+        )}
+      </Container>
+    </Menu>
+  );
 }
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import App from "next/app";
+import Layout from "../components/_App/Layout";
 
 class MyApp extends App {
   render() {
     const { Component } = this.props;
-    return <Component />;
+    return (
+      <Layout>
+        <Component />
+      </Layout>
+    );
   }
 }
 
