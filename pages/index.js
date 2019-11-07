6f410dad9dde2dc2531c50205f2e0a38244e3e67
Waga <wagaodongo@gmail.com>
2019-11-07 21:21:20 +0300

    products

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
 function Home() {
+  React.useEffect(()=> {
+    getProducts()
+  }, [])
+
+  function getProducts() {
+
+    const url ='http://localhost:3000/api/products'
+    axios.get(url)
+  }
+
+
   return <>home</>;
 }
 
