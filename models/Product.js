1bce4ce41bc6717d4e27b59dc2b989b5bdfbdff9
Waga <wagaodongo@gmail.com>
2019-11-10 22:28:10 +0300

    populate database

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+import shortid from 'shortid'
+
+const {String, Number}= mongoose.Schema.Types
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    sku:{
+        type: String,
+        unique: true,
+        default: shortid.generate()
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    mediaUrl: {
+        type: String, 
+        required: true
+    }
+
+})
+
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema)
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,8 +1,12 @@
-import products from '../../static/products'
+//import products from '../../static/products'
+
+import Product from '../../models/Product'
 import connectDb from '../../utils/connectDb'
 
 connectDb()
 
-export default (req, res) => {
+export default async (req, res) => {
+ const products = await Product.find()
+
     res.status(200).json(products)
 }
