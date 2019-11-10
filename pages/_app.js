06a7ec8ccfb694e7571d1986b7aa28faf4277094
Waga <wagaodongo@gmail.com>
2019-11-10 21:07:31 +0300

    fetching on the server

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,24 @@ import App from "next/app";
 import Layout from "../components/_App/Layout";
 
 class MyApp extends App {
+static  async getInitialProps({Component, ctx}) {
+
+  let pageProps = {}
+
+  if(Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  return {pageProps}
+
+}
+
+
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
       <Layout>
-        <Component />
+        <Component {...pageProps}/>
       </Layout>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import axios from 'axios'
 
-function Home() {
-  React.useEffect(()=> {
-    getProducts()
-  }, [])
+function Home({products}) {
+console.log(products)
 
-  async function getProducts() {
+  // React.useEffect(()=> {
+  //   getProducts()
+  // }, [])
 
-    const url ='http://localhost:3000/api/products'
-    const response = await axios.get(url)
-    console.log(response.data)
-  }
+  // async function getProducts() {
+
+  //   const url ='http://localhost:3000/api/products'
+  //   const response = await axios.get(url)
+  //   console.log(response.data)
+  // }
 
 
   return <>home</>;
+}
+
+Home.getInitialProps =  async () => {
+//fetch data from server
+const url ='http://localhost:3000/api/products'
+const response = await axios.get(url)
+return {products: response.data}
+
+//return response data as an object
+return {hello: 'world'}
+
+//note: this object will be merged with existing props
+
 }
 
 export default Home;
