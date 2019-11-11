d3b58fb60d628718881ca082f284d88677dd0014
Waga <wagaodongo@gmail.com>
2019-11-11 21:19:37 +0300

    product summary

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,5 +1,24 @@
-function ProductSummary() {
-  return <>ProductSummary</>;
+import {Item, Label} from 'semantic-ui-react'
+
+function ProductSummary({name, mediaUrl, _id, price, sku}) {
+  return(
+    <Item.Group>
+      <Item>
+      <Item.Image size ="medium" src={mediaUrl} />
+      <Item.Content>
+<Item.Header>{name}</Item.Header>
+<Item.Description>
+<p>${price}</p>
+  <Label>SKU: {sku}</Label>
+</Item.Description>
+      </Item.Content>
+
+      </Item>
+
+    </Item.Group>
+  )
 }
+
+
 
 export default ProductSummary;
--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,24 @@
-function Product() {
-  return <>product</>;
+import axios from 'axios'
+import ProductSummary from '../components/Product/ProductSummary'
+import ProductAttributes from '../components/Product/ProductAttributes'
+
+function Product({product}) {
+  return (
+    <>
+    <ProductSummary {...product}/>
+  <ProductAttributes {...product}/>
+
+    </>
+  );
 }
+
+Product.getInitialProps = async ({ query: {_id}}) => {
+const url = 'http://localhost:3000/api/product'
+const payload = {params: {_id} }
+const response = await axios.get(url, payload)
+return {product: response.data}
+
+}
+
 
 export default Product;
