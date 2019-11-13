f66cb9472ae136b75869d00ec26d127473789259
Waga <wagaodongo@gmail.com>
2019-11-13 21:36:59 +0300

    delete method

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,15 +1,49 @@
-import {Header, Button} from 'semantic-ui-react'
+import {Header, Button, Modal} from 'semantic-ui-react'
+import React from 'react'
+import baseUrl from '../../utils/baseUrl'
+import axios from 'axios'
 
 function ProductAttributes({ description}) {
-  return <>
+const [modal, setModal] = React.useState(false)
+
+async function handleDelete() {
+  const url = `${baseUrl}/api/product`
+  const payload = {params: {_id}}
+  await axios.delete(url, payload)
+
+}
+
+  return (
+  <>
   <Header as ="h3">About this product </Header>
 <p>{description}</p>
 <Button
 icon="trash alternate outline"
 color="red"
 content="Delete Product"
+onClick= {()=> setModal(true)}
 />
-  </>;
+<Modal open={modal} dimmer="blurring">
+  <Modal.Header>Confirm Delete </Modal.Header>
+  <Modal.Content>
+    Are you sure you want to delete this product?
+  </Modal.Content>
+  <Modal.Actions>
+    <Button content="Cancel" 
+    onClick={()=> setModal(false)}
+    />
+    <Button
+    negative
+    icon="trash"
+    labelPosition="right"
+    content="Delete"
+    onClick={handleDelete}
+    />
+  </Modal.Actions>
+
+</Modal>
+  </>
+  )
 }
 
 export default ProductAttributes;
--- pages/api/product.js
+++ pages/api/product.js
@@ -2,7 +2,29 @@ import Product from '../../models/Product'
 
 
 export default async (req, res) => {
+switch(req.method) {
+    case "GET":
+        await handleGetRequest(req, res);
+        break;
+    case "DELETE":
+        await handleDeleteRequest(req, res);
+        break
+}
+
+}
+
+async function handleGetRequest(req, res) {
     const {_id} = req.query
     const product = await Product.findOne({_id})
     res.status(200).json(product)
 }
+
+async function handleDeleteRequest( req, res) {
+    const {_id} = req.query
+    await Product.findOneAndDelete({_id})
+    res.status(204).json({})
+}
+
+
+
+   
