f13ed5689954c7699ec569ee67aad60142f7e589
Waga <wagaodongo@gmail.com>
2019-11-13 22:46:23 +0300

    Form Submission success

--- pages/create.js
+++ pages/create.js
@@ -1,19 +1,61 @@
+import React from 'react'
 import {Form, Image, TextArea, Button, Message, Header, Icon, Input} from 'semantic-ui-react'
 
+
+const INITIAL_PRODUCT = {
+  name:'',
+  price:'',
+  media:'',
+  description:''
+
+}
+
 function CreateProduct() {
+const [product, setProduct] = React.useState(INITIAL_PRODUCT)
+
+const [mediaPreview, setMediaPreview] = React.useState('')
+const [success, setSuccess] = React.useState(false)
+
+function handleChange(e) {
+  const {name, value, files} =e.target
+ if(name ==="media") {
+   setProduct(prevState => ({...prevState, media: files[0]}))
+   setMediaPreview(window.URL.createObjectURL(files[0]))
+ } else {
+  setProduct((prevState)=> ({...prevState, [name]:value}))
+ }
+ 
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(product)
+  setProduct(INITIAL_PRODUCT)
+  setSuccess(true)
+}
+
   return (
     <>
   <Header as ="h2" block>
     <Icon name= "add" color="orange" />
     Create New Product
   </Header>
-  <Form>
+  <Form success={success} onSubmit={handleSubmit}>
+    <Message 
+    success
+    icon="check"
+    header="Success!"
+    content="Your product has been posted"
+    />
+
     <Form.Group widths="equal">
       <Form.Field
       control ={Input}
       name="name"
       label="Name"
+      value={product.name}
       placeholder="Name"
+      onChange={handleChange}
       />
        
       <Form.Field
@@ -21,28 +63,33 @@ function CreateProduct() {
       name="Price"
       label="Price"
       placeholder="Price"
+      value={product.price}
       min="0"
       step="1"
       type="number"
+      onChange={handleChange}
       />
       
       <Form.Field
       control ={Input}
       name="media"
+      type="file"
       label="Media"
-      placeholder="Name"
       accept="image/*"
       content= "Select Image"
+      onChange={handleChange}
       />
 
 
     </Form.Group>
+    <Image src={mediaPreview} rounded centered size="small" />
    
       <Form.Field
       control ={TextArea}
       name="description"
       label="Description"
       placeholder="Description"
+      value={product.description}
       />
        
       <Form.Field
