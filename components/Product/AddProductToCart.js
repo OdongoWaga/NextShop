befa0223e6038d0f94de6515663799db22a697a5
Waga <wagaodongo@gmail.com>
2019-11-15 14:15:46 +0300

    increasing number of products in cart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,11 +1,16 @@
 import {Input, Icon} from 'semantic-ui-react'
+import React from 'react'
 
 function AddProductToCart() {
+  const [quantity, setQuantity] = React.useState(1)
+
+
   return <Input
     type="number"
     min="1"
     placeholder="Quantity"
-    value = {1}
+    value = {quantity}
+    onChange= {e => setQuantity(Number(e.target.value))}
     action= {
       {
         color:'orange',
