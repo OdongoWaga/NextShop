96b5eeb635dd6dcfd1e92aa292a91705aa94ad11
Waga <wagaodongo@gmail.com>
2019-11-19 05:22:50 +0300

    predeployment setup

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" ? 'https://deployment-url.now.sh': 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === "production" ? 'https://next-shop.now.sh': 'http://localhost:3000';
 
 export default baseUrl;
